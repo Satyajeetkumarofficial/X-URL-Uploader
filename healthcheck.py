a97b18acbd11ee5c2aefe7b128d7fc0793780296
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-08-27 07:56:32 +0530

    Update healthcheck.py

--- healthcheck.py
+++ healthcheck.py
@@ -3,10 +3,18 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class HealthHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == "/health":
+        if self.path in ["/", "/health"]:
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def do_HEAD(self):
+        if self.path in ["/", "/health"]:
+            self.send_response(200)
+            self.end_headers()
         else:
             self.send_response(404)
             self.end_headers()
